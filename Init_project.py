0adc7670593b9394cb7d7cf12c3d8d877c869e9a
Michał Wójcik <michalwojcik741@gmail.com>
2024-10-18 22:20:25 +0200

    Init project

---
+++ Init_project.py
@@ -0,0 +1,6 @@
+print("Lista zakupów")
+Piekarnia = ['Chleb', "Pączek", "Bułki"]
+Warzywniak = ["Marchew", "Seler", "Rucola"]
+print("Idę do Piekarnia i kupuje tam" ,Piekarnia)
+print("Idę do Warzywniak i kupuje tam" ,Warzywniak)
+print("W sumie kupuję",len(Piekarnia + Warzywniak),"produktów")
